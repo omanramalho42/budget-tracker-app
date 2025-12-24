e3f6358bf2c8e3cd3291ef774088789aecbb7dd3
Oman <omanapple42@hotmail.com>
2025-12-24 19:05:39 -0300

    fix/make a inspect error on database envirnment not found

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -10,7 +10,10 @@ declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>
 } & typeof global
 
-const connectionString = `${process.env.DATABASE_URL}`
+const connectionString = process.env.DATABASE_URL
+if (!connectionString) {
+  throw new Error('DATABASE_URL environment variable is not set')
+}
 
 const adapter = new PrismaPg({ connectionString })
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
