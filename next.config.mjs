d421fa2e89c33e40e4ede40f75a617d0fa5bbf2c
Oman <omanapple42@hotmail.com>
2026-04-15 15:46:02 -0300

    feat/upsize limit image

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+   experimental: {
+    serverActions: {
+      bodySizeLimit: '20mb',
+    },
+    // 🔥 ISSO AQUI resolve o erro de 10MB
+    api: {
+      bodyParser: {
+        sizeLimit: "20mb",
+      },
+    },
+  },
   eslint: {
     ignoreDuringBuilds: true,
 }, typescript: {
